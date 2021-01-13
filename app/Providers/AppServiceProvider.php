d7ef8a7b248ecbcb29ca65870b7fdb701c675070
Rasel Ahmed <rsl.ahmed383@gmail.com>
2021-01-13 22:49:09 +0600

    “wip”

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,16 +15,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Force HTTPS when REDIRECT_HTTPS=true in .env
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $urlGenerator
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $urlGenerator)
     {
-        //
+        // Force HTTPS when REDIRECT_HTTPS=true in .env
+        if (env('REDIRECT_HTTPS')) {
+            $urlGenerator->formatScheme('https');
+        }
+        Schema::defaultStringLength(191);
     }
 }
